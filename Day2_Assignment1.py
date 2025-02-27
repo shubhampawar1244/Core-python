7fd0033a3cc9aba831b77b78d9de305b823cfd54
shubhampawar1244 <sp7250007@gmail.com>
2025-02-27 13:39:23 +0530

    Add files via upload

---
+++ Day2_Assignment1.py
@@ -0,0 +1,16 @@
+#program to  calculate the multiplication and sum of two numbers
+#Enter first number
+num1=float(input("Enter first number"))
+#Enter second number
+num2=float(input("Enter second number"))
+#perform operation
+mult=num1*num2
+add=num1+num2
+#display result
+print("Multiplication:",mult)
+print("Addition:",add)
+''' OUTPUT:
+Enter first number11
+Enter second number44
+Multiplication: 484.0
+Addition: 55.0 '''
